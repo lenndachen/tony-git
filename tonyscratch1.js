831eeeabfce4cfae8852c1b502e484a4c99ea83a
Linda Chen <lindachen548@gmail.com>
2019-04-23 01:49:05 +0000

    tonyscratch1 add

---
+++ tonyscratch1.js
@@ -0,0 +1,23 @@
+var name = prompt("Please tell me your name."); 
+var userReply = prompt(" ")
+var tonyReply = (Tony)
+
+
+
+
+selfArray = [hurt self, cut myself,]
+abuseArray = [bitch, dad hurt m]
+
+
+
+
+switch (prompt("Hello " ${name} " please explain how you are feeling today"))
+
+
+
+
+for (tony )
+if (#arrays are contained withing userReply)
+    then tonyReply = Tony.selfArray
+    else tonyReply = Tony. abuseArray
+    else if to)
