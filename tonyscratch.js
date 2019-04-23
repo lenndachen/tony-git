0630ff5f3c649a44c755265c797278ef32639a0a
Linda Chen <lindachen548@gmail.com>
2019-04-23 01:46:05 +0000

    tonyscratch add

---
+++ tonyscratch.js
@@ -0,0 +1,24 @@
+var name = prompt("Please tell me your name."); 
+var name = response(" ")
+
+switch (prompt("Hello " ${name} " please explain how you are feeling today")) {
+  case 'Depression, suicide ideation, hopeless, sleeping a lot':
+    console.log('Based on your description, it sounds like you are depression.');
+    break;
+}
+
+/*
+*catagories- is this like this other term
+ - Depression
+    - Suicide Ideation, 
+ -Mental Illness
+    - bipolar, ptsd, 
+ -Feelings
+    - sad - rating
+    - happy - rating
+    - ashamed - rating
+*/
+
+
+
+var depression = [reckless behavior, suicidal tendency, sadness]
